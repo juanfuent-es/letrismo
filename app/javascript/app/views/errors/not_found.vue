3a6e4862f551c8320618135d0a292811ba65d01a
JuanFuent.es <input@juanfuent.es>
2023-11-04 11:48:30 -0600

    Remove Old Vue Files

--- app/javascript/app/views/errors/not_found.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <section class="w-full relative items-center h-screen flex bg-black text-white">
-        <article class="w-full text-center absolute z-20">
-            <h1 class="block text-8xl">Error 404</h1>
-            <p class="w-full text-xl">{{ $t('not_found') }}</p>
-        </article>
-    </section>
-</template>
---
+++ app/javascript/components/page.js
@@ -0,0 +1,38 @@
+export default class Page {
+    constructor() {
+        this.turbo()
+        this.mount()
+    }
+
+    turbo() {
+        // https://turbo.hotwired.dev/reference/events
+        document.addEventListener("turbo:click", async (event) => this.unmount(event))
+        document.addEventListener("turbo:frame-load", async (event) => {
+            this.mount()
+        })
+    }
+
+    mount() {
+        gsap.to("#turbo-frame", {
+            opacity: 1,
+            onComplete: () => {
+                // this.blazy()
+            }
+        })
+    }
+    
+    unmount(e) {
+        e.preventDefault()
+        gsap.to("#turbo-frame", {
+            opacity: 0,
+            onComplete: () => {
+                window.ScrollTo(0, 0)
+            }
+        })
+    }
+
+    // blazy() {
+    //     return new Blazy()
+    // }
+
+}
